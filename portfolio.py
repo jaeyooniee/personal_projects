4e392100cf34b468e50cd95eb65b9ab706c5a552
Jaeyoon Chung <jaeyoon.chung@student.manchester.ac.uk>
2025-12-05 14:06:20 +0000

    Revised comments

--- portfolio.py
+++ portfolio.py
@@ -8,9 +8,11 @@ import datetime
 stock_ticker = ["000660.KS", "015760.KS", "278470.KS", "ARKG", "SGOV", "OKLO", "IONQ", "RKLB"]
 benchmark_ticker = "OEF" # The reason I chose to use S&P100 over S&P500 - https://github.com/jaeyooniee/personal_projects/tree/snp-analysis
 
+
 # 2. Investment Amounts (KRW for Korean, USD for U.S.)
 investment_KRW = np.array([500000, 2000000, 2000000]) 
 investment_USD = np.array([300, 3000, 500, 300, 500])
+
 
 # 3. Download Close Prices
 start_date = "2010-01-01"
@@ -19,15 +21,18 @@ end_date = datetime.datetime.today().strftime("%Y-%m-%d")
 data = yf.download(stock_ticker + [benchmark_ticker], start=start_date, end=end_date, auto_adjust=False)["Close"]
 data = data.dropna()
 
-# 4. Get FX Rate (KRW=X → USD/KRW 환율)
+
+# 4. Get FX Rate (KRW=X → USD/KRW)
 fx = yf.download("KRW=X", start=start_date, end=end_date, auto_adjust=False)["Close"]
-fx = fx.reindex(data.index, method="ffill")  # 날짜 정렬
+fx = fx.reindex(data.index, method="ffill") # sorting the dates
+
 
 # 5. Convert KRW investments to USD
 current_fx = fx.iloc[0].item()
 investment_KRW_to_USD = investment_KRW / current_fx
 
-# 6. Combine weights (USD 기준)
+
+# 6. Combine weights
 investment_total_USD = np.concatenate([investment_KRW_to_USD, investment_USD])
 weights = investment_total_USD / investment_total_USD.sum()
 
@@ -44,12 +49,14 @@ print("\nPortfolio Weights (High → Low):")
 for name, weight in sorted_portfolio:
     print(f"{name}: {weight * 100:.2f}%")
 
+
 # 7. Returns
 returns = data.pct_change().dropna()
 portfolio_returns = returns[stock_ticker].dot(weights)
 
 # Benchmark returns
 benchmark_returns = returns[benchmark_ticker]
+
 
 # 8. Sharpe Ratio
 risk_free_rate = 0.05 / 252
@@ -61,6 +68,7 @@ sharpe_benchmark = (excess_benchmark.mean() / excess_benchmark.std()) * np.sqrt(
 
 print("\nSharpe Ratio (Portfolio USD):", sharpe_portfolio)
 print("Sharpe Ratio (OEF - S&P100 ETF):", sharpe_benchmark)
+
 
 # 9. Cumulative Return Graph Compare
 cumulative_portfolio = (1 + portfolio_returns).cumprod() - 1
